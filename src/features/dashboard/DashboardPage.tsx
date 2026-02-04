fc355b9915d73d4fe28af661f5a16c406ef2525c
bensznaider <bensznaider@yahoo.com.ar>
2026-02-04 09:51:43 -0300

    DashboardPage: error log in bulls API call added

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -47,6 +47,7 @@ export default function DashboardPage() {
     toggleBullFavorite,
     togglingFavorites,
     showFavoritesOnly,
+    error,
   } = useBullsContext();
 
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
@@ -97,6 +98,11 @@ export default function DashboardPage() {
         <CloudSync className="inline-block mr-2" size={16} />
         {lastUpdateText}
       </p>
+      {error && (
+        <div className="bg-red-100 border border-red-400 text-red-700 px-3 py-2 rounded mb-4">
+          {error}
+        </div>
+      )}
       <div className="flex justify-between items-center">
         <h1 className="text-[32px] text-[#2D2D2D] font-semibold">
           Resultados de la clasificación
